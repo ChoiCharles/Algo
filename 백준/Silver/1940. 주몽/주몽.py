f9d28f6ed0df05f328826b7c2ad6a3e39ad8893f
ChoiCharles <122588654+ChoiCharles@users.noreply.github.com>
2023-02-09 17:15:32 +0900

    [Silver IV] Title: 주몽, Time: 128 ms, Memory: 115264 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.py
+++ 백준/Silver/1940. 주몽/주몽.py
@@ -1,21 +1,22 @@
 import sys
+
 input = sys.stdin.readline
 
 n = int(input())
 m = int(input())
-a = list(map(int, input().split()))
-a.sort()
-count = 0
-i = 0
-j = n - 1
-
-while i < j:
-    if a[i] + a[j] < m:
-        i += 1
-    elif a[i] + a[j] > m:
-        j -= 1
+lst = list(map(int, input().split()))
+lst.sort()
+si = 0
+ei = n - 1
+cnt = 0
+while 1:
+    if lst[si] + lst[ei] > m:
+        ei -= 1
+    elif lst[si] + lst[ei] < m:
+        si += 1
     else:
-        count += 1
-        i += 1
-        j -=1
-print(count)
+        cnt += 1
+        si += 1
+    if ei == si:
+        break
+print(cnt)
