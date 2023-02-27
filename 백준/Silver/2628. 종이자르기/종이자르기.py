5454b0924ecd66ae94f2126a27919beb3bfaa389
ChoiCharles <122588654+ChoiCharles@users.noreply.github.com>
2023-02-27 15:38:46 +0900

    [Silver V] Title: 종이자르기, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/2628. 종이자르기/종이자르기.py
+++ 백준/Silver/2628. 종이자르기/종이자르기.py
@@ -3,42 +3,24 @@ input = sys.stdin.readline
 
 m, n = map(int, input().split())
 k = int(input())
-ver = []
-hori = []
+ver = [0]
+hori = [0]
 for i in range(k):
     a, b = map(int, input().split())
     if a == 0:
-        hori.append((a, b))
+        hori.append(b)
     else:
-        ver.append((a, b))
-hori = list(sorted(hori, key = lambda x:x[1]))
-ver = list(sorted(ver, key = lambda x:x[1]))
+        ver.append(b)
+hori.sort()
+ver.sort()
+hori.append(n)
+ver.append(m)
 mh = 0
 mv = 0
 for i in range(1, len(hori)):
-    a = hori[i][1] - hori[i - 1][1]
-    if mh < a:
-        mh = a
+    if mh < hori[i] - hori[i - 1]:
+        mh = hori[i] - hori[i - 1]
 for i in range(1, len(ver)):
-    a = ver[i][1] - ver[i - 1][1]
-    if mv < a:
-        mv = a
-try:
-    if mh < n - hori[-1][1]:
-        mh = n - hori[-1][1]
-    if mh < hori[0][1]:
-        mh = hori[0][1]
-except:
-    pass
-try:
-    if mv < m - ver[-1][1]:
-        mv = m - ver[-1][1]
-    if mv < ver[0][1]:
-        mv = ver[0][1]
-except:
-    pass
-if mv == 0:
-    mv = m
-if mh == 0:
-    mh = n
-print(mv*mh)
+    if mv < ver[i] - ver[i - 1]:
+        mv = ver[i] - ver[i - 1]
+print(mh * mv)
