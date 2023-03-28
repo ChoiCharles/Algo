04ab93df31fae75ddaed04afa259ccfb95d77aa9
ChoiCharles <122588654+ChoiCharles@users.noreply.github.com>
2023-03-28 13:39:09 +0900

    [Silver IV] Title: ATM, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,14 +1,10 @@
-import sys
-input = sys.stdin.readline
-
-def asdf(lst):
-    res = 0
-    for i in range(1, n+1):
-        res += sum(lst[:i])
-    return res
-
 n = int(input())
 lst = list(map(int, input().split()))
 lst.sort()
-ans = asdf(lst)
-print(ans)
+res = 0
+i = 0
+while n:
+    res += lst[i] * n
+    n -= 1
+    i += 1
+print(res)
