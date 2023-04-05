040593ba531637d08cb6baaef324a752e6fa688e
ChoiCharles <122588654+ChoiCharles@users.noreply.github.com>
2023-04-05 17:13:57 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 180 ms, Memory: 34108 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
@@ -7,8 +7,6 @@ res = 0
 for i in range(n):
     a = int(input())
     heapq.heappush(lst, a)
-cnt = heapq.heappop(lst)
-heapq.heappush(lst, cnt)
 cnt = 0
 for i in range(n):
     a = heapq.heappop(lst)
