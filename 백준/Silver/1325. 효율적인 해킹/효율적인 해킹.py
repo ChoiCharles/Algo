9b3aa162e3f45490695363dd348991ddbedb90c3
ChoiCharles <122588654+ChoiCharles@users.noreply.github.com>
2023-04-26 22:06:20 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10668 ms, Memory: 215084 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
@@ -6,30 +6,32 @@ def bfs(a):
     q = deque()
     q.append(a)
     v[a] = 1
+    cnt = 1
     while q:
         a = q.popleft()
         for i in lst[a]:
             if v[i] == 0:
                 q.append(i)
-                res[i] += 1
+                cnt += 1
                 v[i] = 1
+    return cnt
 
 n, m = map(int, input().split())
 lst = [[] for _ in range(n+1)]
-res = [0] * (n + 1)
 
 for i in range(m):
     a, b = map(int, input().split())
-    lst[a].append(b)
-
-for i in range(1, n + 1):
-    v = [0] * (n + 1)
-    bfs(i)
+    lst[b].append(a)
 
 MAX = 0
 for i in range(1, n + 1):
-    MAX = max(MAX, res[i])
+    v = [0] * (n + 1)
+    res = bfs(i)
+    if MAX < res:
+        MAX = res
+        ans = []
+        ans.append(i)
+    elif MAX == res:
+        ans.append(i)
 
-for i in range(1, n + 1):
-    if res[i] == MAX:
-        print(i, end=' ')
+print(*ans)
