d4473646991f3c4c0b02659bedbbbb2ec4b542a6
ChoiCharles <122588654+ChoiCharles@users.noreply.github.com>
2025-04-05 17:06:10 +0900

    [Bronze I] Title: 평균, Time: 212 ms, Memory: 18692 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -3,17 +3,23 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int maxScore = 0;
-		int sum = 0;
 		int n = sc.nextInt();
-		int scoreArray[] = new int[n];
+		
+		int A[] = new int[n];
 		for (int i = 0; i < n; i++) {
-			scoreArray[i] = sc.nextInt();
+			A[i] = sc.nextInt();
 		}
+		
+		int max = 0;
+		long sum = 0;
+		
 		for (int i = 0; i < n; i++) {
-			if (scoreArray[i] > maxScore) maxScore = scoreArray[i];
-			sum += scoreArray[i];
+			if (A[i] > max) {
+				max = A[i];
+			}
+			sum += A[i];
 		}
-		System.out.print(sum * 100.0 / maxScore / n);
+		
+		System.out.print(sum * 100.0 / max / n);
 	}
 }
